eae7866723af7a598a5c2823a382a9b72a105877
Santiago Pereira <santiagoapp@gmail.com>
2024-10-05 15:52:37 -0500

    Unos cambios desde el equipo de Santiago

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Unos cambios fueron agregados desde el equipo de Santiago
         </p>
         <a
           className="App-link"
