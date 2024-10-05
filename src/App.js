5dde585c36c8d2fefa2441a55c86abbb33a69dd0
unmuchachoalto <50554028+unmuchachoalto@users.noreply.github.com>
2024-10-05 16:10:55 -0500

    title

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          unos cambios desde el computador de andres
-        </p>
+        <h1>
+          Lista de Tareas
+        </1>
         <a
           className="App-link"
           href="https://reactjs.org"
